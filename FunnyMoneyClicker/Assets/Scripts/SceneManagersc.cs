ffee80cd6126f835716452c2c7d7e6542bf07fd7
MUKILTEO\1720941 <1720941>
2025-11-07 10:18:56 -0800

    Pc version ui also use this color pallet Classy Palette #5c8357 #3e4a3c #a2af9f #0f849f #00536c

--- FunnyMoneyClicker/Assets/Scripts/SceneManagersc.cs
+++ FunnyMoneyClicker/Assets/Scripts/SceneManagersc.cs
@@ -6,5 +6,18 @@ public class SceneManagersc : MonoBehaviour
    public void LoadSceneByName(string sceneName)
    {
        SceneManager.LoadScene(sceneName);
-    }
+   }
+
+//    public void LoadSceneByName(string sceneName)
+//    {
+//#if PLATFORM_ANDROID
+
+//        SceneManager.LoadScene(sceneName);
+
+//#else
+
+//        SceneManager.LoadScene(sceneName);
+
+//#endif
+//    }
 }
